8f50b5b352ec10484d9c1e1d2ae0bc6d86376976
juneechen <juneechen2021@outlook.com>
2023-04-13 17:58:34 -0400

    add features: get snapshot with index (also ask view to display it), get a list of selection options

--- src/controller/Features.java
+++ src/controller/Features.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.util.List;
+
 public interface Features {
   void exit();
-
+  // void selectSnapshot(String id);
+  void getSnapshot(int i);
+  List<String> getSelectionItems();
 }
