daebc0ba2a1cc7bdbd98a2c3f75f02ad60d478c6
juneechen <juneechen2021@outlook.com>
2023-04-13 18:01:22 -0400

    separate infoPane from Canvas (the graphic Pane)

--- src/views/Canvas.java
+++ src/views/Canvas.java
@@ -1,21 +1,24 @@
 package views;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.*;
 
+
 public class Canvas extends JPanel {
-  private JLabel infoPane;
-  private List<String> shapes;
+  private List<String> shapesInfo;
+  // private List<IShape> shapes;
 
   public Canvas() {
     this.setOpaque(true);
     this.setBackground(new Color(60, 120, 120));
 
-    this.shapes = new ArrayList<>();
+    this.shapesInfo = new ArrayList<>();
+    // this.shapes = new ArrayList<>();
   }
 
   @Override
@@ -25,7 +28,7 @@ public class Canvas extends JPanel {
     g.setColor(Color.PINK);
     g.fillOval(60, 60, 120, 80);
 
-    for (String each : this.shapes) {
+    for (String each : this.shapesInfo) {
       Scanner scan = new Scanner(each);
       String shape = scan.next();
       int x = scan.nextInt();
@@ -47,10 +50,44 @@ public class Canvas extends JPanel {
     }
   }
 
+//    @Override
+//    protected void paintComponent(Graphics g) {
+//      super.paintComponent(g);
+//      for (IShape each : shapes) {
+//        drawShape(each.copy());
+//      }
+//    }
 
-  public void renderSnapshot(List<String> shapesInfo) {
-    this.shapes = new ArrayList<>();
-    shapes.addAll(shapesInfo);
-    this.repaint();
+  public void addShape(String info) {
+    shapesInfo.add(info);
   }
+
+//
+//  public void renderSnapshot(List<String> shapesInfo) {
+//    this.shapesInfo = new ArrayList<>();
+//    this.shapesInfo.addAll(shapesInfo);
+//    this.repaint();
+//  }
+
+//  public void renderShapes(List<IShape> shapes) {
+//    this.shapes = new ArrayList<>(shapes);
+//
+//    this.repaint();
+//  }
+//
+//  private void drawShape(IShape r) {
+//
+//  }
+//
+//  private void drawShape(model.Rectangle r) {
+//    Graphics g = this.getGraphics();
+//    g.setColor(r.getColor());
+//    g.fillRect(r.getPosition().getX(), r.getPosition().getY(), r.getXDimension(), r.getYDimension());
+//  }
+//
+//  private void drawShape(model.Oval r) {
+//    Graphics g = this.getGraphics();
+//    g.setColor(r.getColor());
+//    g.fillOval(r.getPosition().getX(), r.getPosition().getY(), r.getXDimension(), r.getYDimension());
+//  }
 }
