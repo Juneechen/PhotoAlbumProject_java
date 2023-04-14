0472aea021a220b1fa31a3a03b5f56f3a5257cc8
juneechen <juneechen2021@outlook.com>
2023-04-14 17:41:42 -0400

    move to view package

--- src/views/IShapeVisitor.java
+++ src/views/IShapeVisitor.java
@@ -1,9 +1,11 @@
-package model;
+package views;
 
+import model.IShape;
 import model.Oval;
 import model.Rectangle;
 
 public interface IShapeVisitor {
   void visit(Rectangle rect);
   void visit(Oval oval);
+  void visit(IShape shape);
 }
